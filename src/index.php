fb603a5638d18fdb73d3763cfb9f3f612a1818b2
abigeater <abigeater@163.com>
2020-12-06 15:46:29 +0800

    update 优化代码

--- src/index.php
+++ src/index.php
@@ -11,37 +11,42 @@ include BASE_DIR.'config/config.php';
 if(empty(CURRENT_PAGE) || empty(CURRENT_POST)){
     $include_file = OUTPUT_PATH.'index.php';
     if(!file_exists($include_file)){
-        $html = '';  
+        $list_html = '';  
         foreach (post_file_list() as $filename) {
             if(empty($filename)) continue;
-            $html .= '<li><a href="/?page=post&post='.$filename.'">'.$filename.'</a></li>';   
+            $list_html .= '<li><a href="/?page=post&post='.$filename.'">'.$filename.'</a></li>';   
         }
-        $buildFile = new BuildFile();
-        $buildFile->setBaseFile(TEMPLATE_PATH.'_list.php');
-        $buildFile->setOutPutFath($include_file);
-        $buildFile->setSearchKey([
-            'list'=>$html
-        ]);
+
+        $template_file = TEMPLATE_PATH.'_list.php';
+        $search_keys   = [
+            'list'=>$list_html
+        ];
     }
 }else{
     $include_file = OUTPUT_PATH.CURRENT_POST.'.php';
     if(!file_exists($include_file)){
         $filename = POST_PATH.'/'.CURRENT_POST.'.md';
+        if(!file_exists($filename)){
+            die("post not found");
+        }
         $file_fd = fopen($filename , "r") or die('Unable to open file');
         $content = fread($file_fd, filesize($filename));
         $parsedown = new Parsedown();
         $content = $parsedown->text($content); 
 
-        $buildFile = new BuildFile();
-        $buildFile->setBaseFile(TEMPLATE_PATH.'_post.php');
-        $buildFile->setOutPutFath($include_file);
-        $buildFile->setSearchKey([
+        $template_file = TEMPLATE_PATH.'_post.php';
+        $search_keys   = [
             'content'=>$content
-        ]);
+        ];
     }
 }
 
-if(!file_exists($include_file)){
+if(isset($template_file)){
+    $buildFile = new BuildFile();
+    $buildFile->setBaseFile($template_file);
+    $buildFile->setOutPutFath($include_file);
+    $buildFile->setSearchKey($search_keys);
+
     $buildPage = new BuildPage(); 
     $buildPage->setBuildClass($buildFile);
     $buildPage->build();
