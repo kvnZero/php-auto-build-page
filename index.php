f90d963a1b2b322f5f90abbd03289951b98bd22c
abigeater <abigeater@163.com>
2020-12-06 15:40:51 +0800

    update 修改自动加载

--- index.php
+++ index.php
@@ -4,10 +4,4 @@ define('APP_PATH', __DIR__ . '/');
 
 include APP_PATH.'/vendor/autoload.php';
 
-spl_autoload_register(function ($class_name) {
-    if(strstr($class_name, 'AutoBuild')){
-        require_once APP_PATH.'/src/'.str_replace('\\','/',$class_name) . '.php';
-    }
-});
-
 include APP_PATH.'/src/index.php';
