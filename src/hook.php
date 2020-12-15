53e7522d60ce0144b593f578bcd7e04017c9b839
abigeater <abigeater@163.com>
2020-12-15 23:37:55 +0800

    update 改善全局标签 并将模版拆分

--- src/hook.php
+++ src/hook.php
@@ -5,14 +5,14 @@ BuildFile::register_common_search_key('include', 'RegisterKey::register');
 
 class RegisterKey
 {
-    public static function register($key, $replace_key, $content, $value){
+    public static function register($key, $replace_key, &$content, $value){
         $method_name = "action_{$key}";
         if(method_exists(RegisterKey::class, $method_name)){
-            $method_name($replace_key, $content, $value);
+            RegisterKey::$method_name($replace_key, $content, $value);
         }
     }
 
-    public static function include($replace_key, $content, $value){
+    public static function action_include($replace_key, &$content, $value){
         if(file_exists(TEMPLATE_PATH.$value)){
             $file_fd = fopen(TEMPLATE_PATH.$value, "r");
             $file_content = fread($file_fd, filesize(TEMPLATE_PATH.$value));
