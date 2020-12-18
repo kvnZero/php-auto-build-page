42bc9c1883a00b7b02b99db798f4b4c9e1d320d5
sohyunQVQ <abigeater@163.com>
2020-12-18 18:52:28 +0800

    update head同理

--- src/template/_head.php
+++ src/template/_head.php
@@ -4,7 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="/static/css/prism.css">
+    {% if !empty(CURRENT_POST) then <link rel="stylesheet" href="/static/css/prism.css"> %}
     <title>Abigeater😊 Home Page</title>
     <style>
     ul li{
